ac32ecb0e76a54fe2b2c288860ad9f2702f0ad65
jaffar-one <jaffar.inn@gmail.com>
2022-04-27 12:25:28 +0530

    first commit

---
+++ Pod4 (1)/Pod4dd/OnineBanking1/src/test/java/com/cts/onlinebanking/OnineBankingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cts.onlinebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnineBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+	}
+
+}
